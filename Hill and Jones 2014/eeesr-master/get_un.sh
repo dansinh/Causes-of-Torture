c2ae42720dc2977c58161b4325f2d33abbe7e5fc
Dan Sinh <dansinh@gmail.com>
2018-01-23 12:23:05 -0500

    Removed folder from repository

--- Hill and Jones 2014/eeesr-master/get_un.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/zmjones/untreaties.git
-mv untreaties/treaties/4-9.csv data/cat.csv
-mv untreaties/treaties/4-4.csv data/cpr.csv
-mv untreaties/utilities.R un_utilities.R
-rm -rf ./untreaties
